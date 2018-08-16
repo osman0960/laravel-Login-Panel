81acac5df26abc598e511822c64164a14772a4c9
osman0960 <osmanforhad@haotmail.com>
2018-08-16 18:33:58 +0600

    laravel Login Panel

---
+++ laravel-Login-Panel/Db/enrolment.sql
@@ -0,0 +1,102 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 16, 2018 at 02:37 AM
+-- Server version: 10.1.33-MariaDB
+-- PHP Version: 7.2.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `enrolment`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin_tbl`
+--
+
+CREATE TABLE `admin_tbl` (
+  `admin_id` int(10) UNSIGNED NOT NULL,
+  `admin_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `admin_email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `admin_password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `admin_phone` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `admin_tbl`
+--
+
+INSERT INTO `admin_tbl` (`admin_id`, `admin_name`, `admin_email`, `admin_password`, `admin_phone`, `created_at`, `updated_at`) VALUES
+(1, 'osman', 'osmanforhad93@gmail.com', '817b618be26e9ad1034392949d55309d', '01531531576', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2018_08_15_010649_create_admin_tbl_table', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin_tbl`
+--
+ALTER TABLE `admin_tbl`
+  ADD PRIMARY KEY (`admin_id`);
+
+--
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin_tbl`
+--
+ALTER TABLE `admin_tbl`
+  MODIFY `admin_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ laravel-Login-Panel/enrolment/resources/views/admin/dashboard.blade.php
@@ -0,0 +1,46 @@
+@extends('layout')
+@section('content')
+
+ <div class="col-sm-6 col-md-3 grid-margin">
+              <div class="card">
+                <div class="card-body">
+                  <h2 class="card-title">All Sudents</h2>
+                </div>
+                <div class="dashboard-chart-card-container">
+                  <div id="dashboard-card-chart-1" class="card-float-chart"></div>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-6 col-md-3 grid-margin">
+              <div class="card">
+                <div class="card-body">
+                  <h2 class="card-title">All teachers</h2>
+                </div>
+                <div class="dashboard-chart-card-container">
+                  <div id="dashboard-card-chart-2" class="card-float-chart"></div>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-6 col-md-3 grid-margin">
+              <div class="card">
+                <div class="card-body">
+                  <h2 class="card-title">Tution Fee</h2>
+                </div>
+                <div class="dashboard-chart-card-container">
+                  <div id="dashboard-card-chart-3" class="card-float-chart"></div>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-6 col-md-3 grid-margin">
+              <div class="card">
+                <div class="card-body">
+                  <h2 class="card-title">Revenue</h2>
+                </div>
+                <div class="dashboard-chart-card-container">
+                  <div id="dashboard-card-chart-4" class="card-float-chart"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+         
+@endsection
